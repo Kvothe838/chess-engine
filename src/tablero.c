420f70b5f59d13d2d6594b7e011711039d3dcf64
Facundo <facundo@omniasalud.com>
2020-01-04 20:36:18 -0300

    Pasada la lógica a donde debería estar, en el tablero.c. Esto no funciona porque faltan mergear los cambios del makefile para poder llamar desde main a tablero.c.

--- src/tablero.c
+++ src/tablero.c
@@ -4,13 +4,67 @@
 #define FILAS 8
 #define COLUMNAS 8
 
-// void imprimirTablero(int tablero[FILAS][COLUMNAS])
-// {
-//     for (int i = 0; i < FILAS; i++)
-//     {
-//         for (int j = 0; j < COLUMNAS; j++)
-//         {
-//             printf("%c", tablero[i][j]);
-//         }
-//     }
-// }
+#define ALFIL_BLANCO "AB"
+#define ALFIL_NEGRO "AN"
+#define CABALLO_BLANCO "CB"
+#define CABALLO_NEGRO "CN"
+#define PEON_BLANCO "PB"
+#define PEON_NEGRO "PN"
+#define REINA_BLANCO "RB"
+#define REINA_NEGRO "RN"
+#define REY_BLANCO "YB"
+#define REY_NEGRO "YN"
+#define TORRE_BLANCO "TB"
+#define TORRE_NEGRO "TN"
+#define NADA "  "
+
+void imprimirTablero(char*** tablero)
+{
+    printf("\n—————————————————————————————————————————\n");
+    
+    for (int i = 0; i < FILAS; i++)
+    {
+        printf("|");
+
+        for (int j = 0; j < COLUMNAS; j++)
+        {
+            printf(" %s |", tablero[i][j]);            
+        }
+
+        printf("\n—————————————————————————————————————————\n");
+    }
+}
+
+char*** obtenerTableroInicial()
+{
+    char tableroProvisorio[FILAS][COLUMNAS][3] = 
+    {
+        {PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO},
+        {TORRE_BLANCO, CABALLO_BLANCO, ALFIL_BLANCO, REY_BLANCO, REINA_BLANCO, ALFIL_BLANCO, CABALLO_BLANCO, TORRE_BLANCO},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {TORRE_NEGRO, CABALLO_NEGRO, ALFIL_NEGRO, REY_NEGRO, REINA_NEGRO, ALFIL_NEGRO, CABALLO_NEGRO, TORRE_NEGRO},
+        {PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO}
+    };
+
+    char*** tablero = (char***) malloc(FILAS * sizeof(char**));
+
+    for (int i = 0; i < FILAS; i++)
+    {
+        tablero[i] = (char**) malloc(COLUMNAS * sizeof(char*));
+
+        for (int j = 0; j < COLUMNAS; j++)
+        {
+            tablero[i][j] = (char*) malloc(COLUMNAS * sizeof(char));
+
+            for (int k = 0; k < 3; k++)
+            {
+                tablero[i][j][k] = tableroProvisorio[i][j][k];
+            }
+        }
+    }
+
+    return tablero;
+}
