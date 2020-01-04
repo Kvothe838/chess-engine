6fe553e0fdda90a08191461632c85fd1721841c8
Facundo <facundo@omniasalud.com>
2020-01-04 20:33:23 -0300

    Agregada lógica para mostrar tablero en tablero.c e inicializar el tablero. Agregado .vscode/ al .gitignore.

---
+++ src/tablero.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define FILAS 8
+#define COLUMNAS 8
+
+// void imprimirTablero(int tablero[FILAS][COLUMNAS])
+// {
+//     for (int i = 0; i < FILAS; i++)
+//     {
+//         for (int j = 0; j < COLUMNAS; j++)
+//         {
+//             printf("%c", tablero[i][j]);
+//         }
+//     }
+// }
