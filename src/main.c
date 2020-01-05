8059891e23490a668e2f3f3c01a1c47ac0d751fb
Facundo <facundo@omniasalud.com>
2020-01-04 22:06:38 -0300

    Limpieza de Tablero. Beautiful.

--- src/main.c
+++ src/main.c
@@ -4,26 +4,13 @@
 
 int main(void)
 {
-    char*** tablero = TableroInicializar();
+    Tablero tablero;
+
+    TableroInicializar(&tablero);
 
     TableroImprimir(tablero);
 
-    FreeTablero(tablero);    
+    TableroDestruir(tablero);
 
     return EXIT_SUCCESS;
-}
-
-void FreeTablero(char*** tablero)
-{
-    for (int i = 0; i < FILAS; i++)
-    {
-        for (int j = 0; j < COLUMNAS; j++)
-        {
-            free(tablero[i][j]);
-        }
-
-        free(tablero[i]);
-    }
-
-    free(tablero);
 }
--- src/tablero.c
+++ src/tablero.c
@@ -1,24 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<tablero.h>
 
-#define FILAS 8
-#define COLUMNAS 8
+void TableroInicializar(Tablero* tablero)
+{
+    char tableroProvisorio[FILAS][COLUMNAS][3] = 
+    {
+        {PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO},
+        {TORRE_BLANCO, CABALLO_BLANCO, ALFIL_BLANCO, REY_BLANCO, REINA_BLANCO, ALFIL_BLANCO, CABALLO_BLANCO, TORRE_BLANCO},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {TORRE_NEGRO, CABALLO_NEGRO, ALFIL_NEGRO, REY_NEGRO, REINA_NEGRO, ALFIL_NEGRO, CABALLO_NEGRO, TORRE_NEGRO},
+        {PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO}
+    };
 
-#define ALFIL_BLANCO "AB"
-#define ALFIL_NEGRO "AN"
-#define CABALLO_BLANCO "CB"
-#define CABALLO_NEGRO "CN"
-#define PEON_BLANCO "PB"
-#define PEON_NEGRO "PN"
-#define REINA_BLANCO "RB"
-#define REINA_NEGRO "RN"
-#define REY_BLANCO "YB"
-#define REY_NEGRO "YN"
-#define TORRE_BLANCO "TB"
-#define TORRE_NEGRO "TN"
-#define NADA "  "
+    *tablero = (Tablero) malloc(FILAS * sizeof(char**));
 
-void TableroImprimir(char*** tablero)
+    for (int i = 0; i < FILAS; i++)
+    {
+        (*tablero)[i] = (char**) malloc(COLUMNAS * sizeof(char*));
+
+        for (int j = 0; j < COLUMNAS; j++)
+        {
+            (*tablero)[i][j] = (char*) malloc(COLUMNAS * sizeof(char));
+
+            for (int k = 0; k < 3; k++)
+            {
+                (*tablero)[i][j][k] = tableroProvisorio[i][j][k];
+            }
+        }
+    }
+}
+
+void TableroImprimir(Tablero tablero)
 {
     printf("\n—————————————————————————————————————————\n");
     
@@ -35,36 +51,17 @@ void TableroImprimir(char*** tablero)
     }
 }
 
-char*** TableroInicializar()
+void TableroDestruir(Tablero tablero)
 {
-    char tableroProvisorio[FILAS][COLUMNAS][3] = 
-    {
-        {PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO},
-        {TORRE_BLANCO, CABALLO_BLANCO, ALFIL_BLANCO, REY_BLANCO, REINA_BLANCO, ALFIL_BLANCO, CABALLO_BLANCO, TORRE_BLANCO},
-        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
-        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
-        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
-        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
-        {TORRE_NEGRO, CABALLO_NEGRO, ALFIL_NEGRO, REY_NEGRO, REINA_NEGRO, ALFIL_NEGRO, CABALLO_NEGRO, TORRE_NEGRO},
-        {PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO}
-    };
-
-    char*** tablero = (char***) malloc(FILAS * sizeof(char**));
-
     for (int i = 0; i < FILAS; i++)
     {
-        tablero[i] = (char**) malloc(COLUMNAS * sizeof(char*));
-
         for (int j = 0; j < COLUMNAS; j++)
         {
-            tablero[i][j] = (char*) malloc(COLUMNAS * sizeof(char));
-
-            for (int k = 0; k < 3; k++)
-            {
-                tablero[i][j][k] = tableroProvisorio[i][j][k];
-            }
+            free(tablero[i][j]);
         }
+
+        free(tablero[i]);
     }
 
-    return tablero;
+    free(tablero);
 }
