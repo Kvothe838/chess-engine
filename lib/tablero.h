c053db1b9ccd01e49281aff5b147651c55a5d677
Kvothe838 <facundoyuffrida@gmail.com>
2020-01-10 14:41:05 -0300

    WIP Pieza.

--- lib/tablero.h
+++ lib/tablero.h
@@ -1,24 +1,60 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdbool.h>
+
 #define FILAS 8
 #define COLUMNAS 8
 
-#define ALFIL_BLANCO "AB"
-#define ALFIL_NEGRO "AN"
-#define CABALLO_BLANCO "CB"
-#define CABALLO_NEGRO "CN"
-#define PEON_BLANCO "PB"
-#define PEON_NEGRO "PN"
-#define REINA_BLANCO "RB"
-#define REINA_NEGRO "RN"
-#define REY_BLANCO "YB"
-#define REY_NEGRO "YN"
-#define TORRE_BLANCO "TB"
-#define TORRE_NEGRO "TN"
-#define NADA "  "
+// #define ALFIL 'A'
+// #define CABALLO 'C'
+// #define PEON 'P'
+// #define REINA 'R'
+// #define REY 'K'
+// #define TORRE 'T'
+// #define NADA ' '
 
-typedef char*** Tablero;
+typedef enum {
+    ALFIL = 'A',
+    CABALLO = 'C',
+    PEON = 'P',
+    REINA = 'R',
+    REY = 'K',
+    TORRE = 'T',
+    NADA = ' '
+} TipoPieza;
+
+typedef enum {
+    A = 'A',
+    B = 'B',
+    C = 'C',
+    D = 'D',
+    E = 'E',
+    F = 'F',
+    G = 'G',
+    H = 'H'
+} Columna;
+
+typedef enum {
+    UNO = 1,
+    DOS = 2,
+    TRES = 3,
+    CUATRO = 4,
+    CINCO = 5,
+    SEIS = 6,
+    SIETE = 7,
+    OCHO = 8
+} Fila;
+
+typedef struct {
+    TipoPieza tipo;
+    bool esBlanca;
+    Columna columnaActual;
+    Fila filaActual;
+    //función diciendo si se puede mover a tal lugar?
+} Pieza;
+
+typedef Pieza** Tablero;
 
 void TableroInicializar(Tablero* tablero);
 void TableroImprimir(Tablero tablero);
--- src/tablero.c
+++ src/tablero.c
@@ -2,34 +2,84 @@
 #include<stdlib.h>
 #include<tablero.h>
 
+#define CANTIDAD_PIEZAS 32
+
+void CrearPieza(Pieza* pieza, TipoPieza tipo, bool esBlanca, Columna columna, Fila fila);
+char TraducirColumnaAIndice(Columna columna);
+
 void TableroInicializar(Tablero* tablero)
 {
-    char tableroProvisorio[FILAS][COLUMNAS][3] = 
-    {
-        {PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO},
-        {TORRE_BLANCO, CABALLO_BLANCO, ALFIL_BLANCO, REY_BLANCO, REINA_BLANCO, ALFIL_BLANCO, CABALLO_BLANCO, TORRE_BLANCO},
-        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
-        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
-        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
-        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
-        {TORRE_NEGRO, CABALLO_NEGRO, ALFIL_NEGRO, REY_NEGRO, REINA_NEGRO, ALFIL_NEGRO, CABALLO_NEGRO, TORRE_NEGRO},
-        {PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO}
+    //Guasada monumental.
+    Pieza peonBlanco1, peonBlanco2, peonBlanco3, peonBlanco4, peonBlanco5, peonBlanco6, peonBlanco7, peonBlanco8, 
+    torreBlanco1, torreBlanco2, 
+    caballoBlanco1, caballoBlanco2, 
+    alfilBlanco1, alfilBlanco2, 
+    reyBlanco, reinaBlanco,
+    peonNegro1, peonNegro2, peonNegro3, peonNegro4, peonNegro5, peonNegro6, peonNegro7, peonNegro8, 
+    torreNegro1, torreNegro2, 
+    caballoNegro1, caballoNegro2,
+    alfilNegro1, alfilNegro2,
+    reyNegro, reinaNegro;    
+
+    CrearPieza(&peonBlanco1, PEON, true, G, UNO);
+    CrearPieza(&peonBlanco2, PEON, true, G, DOS);
+    CrearPieza(&peonBlanco3, PEON, true, G, TRES);
+    CrearPieza(&peonBlanco4, PEON, true, G, CUATRO);
+    CrearPieza(&peonBlanco5, PEON, true, G, CINCO);
+    CrearPieza(&peonBlanco6, PEON, true, G, SEIS);
+    CrearPieza(&peonBlanco7, PEON, true, G, SIETE);
+    CrearPieza(&peonBlanco8, PEON, true, G, OCHO);
+    
+    CrearPieza(&torreBlanco1, TORRE, true, H, UNO);
+    CrearPieza(&torreBlanco1, TORRE, true, H, OCHO);
+    CrearPieza(&caballoBlanco1, CABALLO, true, H, DOS);
+    CrearPieza(&caballoBlanco2, CABALLO, true, H, SIETE);
+    CrearPieza(&alfilBlanco1, ALFIL, true, H, TRES);
+    CrearPieza(&alfilBlanco2, ALFIL, true, H, SEIS);
+    CrearPieza(&reyBlanco, REY, true, H, CUATRO);
+    CrearPieza(&reinaBlanco, REINA, true, H, CINCO);
+
+    CrearPieza(&peonNegro1, PEON, false, B, UNO);
+    CrearPieza(&peonNegro2, PEON, false, B, DOS);
+    CrearPieza(&peonNegro3, PEON, false, B, TRES);
+    CrearPieza(&peonNegro4, PEON, false, B, CUATRO);
+    CrearPieza(&peonNegro5, PEON, false, B, CINCO);
+    CrearPieza(&peonNegro6, PEON, false, B, SEIS);
+    CrearPieza(&peonNegro7, PEON, false, B, SIETE);
+    CrearPieza(&peonNegro8, PEON, false, B, OCHO);
+    
+    CrearPieza(&torreNegro1, TORRE, false, A, UNO);
+    CrearPieza(&torreNegro2, TORRE, false, A, OCHO);
+    CrearPieza(&caballoNegro1, CABALLO, false, A, DOS);
+    CrearPieza(&caballoNegro2, CABALLO, false, A, SIETE);
+    CrearPieza(&alfilNegro1, ALFIL, false, A, TRES);
+    CrearPieza(&alfilNegro2, ALFIL, false, A, SEIS);
+    CrearPieza(&reyNegro, REY, false, A, CUATRO);
+    CrearPieza(&reinaNegro, REINA, false, A, CINCO);
+
+    Pieza piezas[CANTIDAD_PIEZAS] = {
+        peonBlanco1, peonBlanco2, peonBlanco3, peonBlanco4, peonBlanco5, peonBlanco6, peonBlanco7, peonBlanco8,
+        torreBlanco1, torreBlanco2, caballoBlanco1, caballoBlanco2, alfilBlanco1, alfilBlanco2, reyBlanco, reinaBlanco,
+        peonNegro1, peonNegro2, peonNegro3, peonNegro4, peonNegro5, peonNegro6, peonNegro7, peonNegro8,
+        torreNegro1, torreNegro2, caballoNegro1, caballoNegro2, alfilNegro1, alfilNegro2, reyNegro, reinaNegro
     };
 
-    *tablero = (Tablero) malloc(FILAS * sizeof(char**));
+    Pieza tableroProvisorio[FILAS][COLUMNAS];
+
+    for(int i = 0; i < CANTIDAD_PIEZAS; i++) {
+        char columnaIndice = TraducirColumnaAIndice(piezas[i].columnaActual);
+        tableroProvisorio[piezas[i].filaActual][columnaIndice] = piezas[i];
+    }
+
+    *tablero = (Tablero) malloc(FILAS * sizeof(Pieza*));
 
     for (int i = 0; i < FILAS; i++)
     {
-        (*tablero)[i] = (char**) malloc(COLUMNAS * sizeof(char*));
+        (*tablero)[i] = (Pieza*) malloc(COLUMNAS * sizeof(Pieza));
 
         for (int j = 0; j < COLUMNAS; j++)
         {
-            (*tablero)[i][j] = (char*) malloc(COLUMNAS * sizeof(char));
-
-            for (int k = 0; k < 3; k++)
-            {
-                (*tablero)[i][j][k] = tableroProvisorio[i][j][k];
-            }
+            (*tablero)[i][j] = tableroProvisorio[i][j];
         }
     }
 }
@@ -44,7 +94,7 @@ void TableroImprimir(Tablero tablero)
 
         for (int j = 0; j < COLUMNAS; j++)
         {
-            printf(" %s |", tablero[i][j]);            
+            printf(" %s |", tablero[i][j].tipo + (tablero[i][j].esBlanca ? "B" : "N"));            
         }
 
         printf("\n—————————————————————————————————————————\n");
@@ -55,13 +105,50 @@ void TableroDestruir(Tablero tablero)
 {
     for (int i = 0; i < FILAS; i++)
     {
-        for (int j = 0; j < COLUMNAS; j++)
-        {
-            free(tablero[i][j]);
-        }
+        // for (int j = 0; j < COLUMNAS; j++)
+        // {
+        //     free(tablero[i][j]);
+        // }
 
         free(tablero[i]);
     }
 
     free(tablero);
+}
+
+void CrearPieza(Pieza* pieza, TipoPieza tipo, bool esBlanca, Columna columna, Fila fila) {
+    pieza->tipo = tipo;
+    pieza->esBlanca = esBlanca;
+    pieza->columnaActual = columna;
+    pieza->filaActual = fila;
+}
+
+char TraducirColumnaAIndice(Columna columna) {
+    switch (columna)
+    {
+    case A:
+        return 0;
+        break;    
+    case B:
+        return 1;
+        break;
+    case C:
+        return 2;
+        break;
+    case D:
+        return 3;
+        break;
+    case E:
+        return 4;
+        break;
+    case F:
+        return 5;
+        break;
+    case G:
+        return 6;
+        break;
+    case H:
+        return 7;
+        break;
+    }
 }
