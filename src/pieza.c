809af0974148bc8c27bcf26e02067fd81a41a5a9
MauricioFIUBA <mlcabrera@fi.uba.ar>
2020-01-11 18:33:37 -0300

    Implemtacion de una funcion para copiar una pieza a otra

--- src/pieza.c
+++ src/pieza.c
@@ -1,9 +1,15 @@
 #include <pieza.h>
 
 void PiezaCrear(Pieza* pieza, TipoPieza tipo, bool esBlanca, Columna columna, Fila fila) {
-    pieza->tipo = (TipoPieza)tipo;
+    pieza->tipo = tipo;
     pieza->esBlanca = esBlanca;
-    pieza->esPieza = true; //Lo más sad que vas a ver hoy.
-    pieza->columnaActual = (Columna)columna;
-    pieza->filaActual = (Fila)fila;
+    pieza->columnaActual = columna;
+    pieza->filaActual = fila;
+}
+
+void PiezaCopiar(Pieza* copia, Pieza original) {
+    copia->tipo = original.tipo;
+    copia->esBlanca = original.esBlanca;
+    copia->columnaActual = original.columnaActual;
+    copia->filaActual = original.filaActual;
 }
