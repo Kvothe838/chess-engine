69034c14d4fec8e5ff74cdb93cacaf1265ee7284
MauricioFIUBA <mlcabrera@fi.uba.ar>
2020-01-11 23:30:22 -0300

    Regresion, volvimos a la forma de inicializacion del tablero original

--- lib/tablero.h
+++ lib/tablero.h
@@ -5,6 +5,20 @@
 #include <pieza.h>
 #include <posicion.h>
 
+#define ALFIL_BLANCO "AB"
+#define ALFIL_NEGRO "AN"
+#define CABALLO_BLANCO "CB"
+#define CABALLO_NEGRO "CN"
+#define PEON_BLANCO "PB"
+#define PEON_NEGRO "PN"
+#define REINA_BLANCO "RB"
+#define REINA_NEGRO "RN"
+#define REY_BLANCO "YB"
+#define REY_NEGRO "YN"
+#define TORRE_BLANCO "TB"
+#define TORRE_NEGRO "TN"
+#define NADA "  "
+
 #define FILAS 8
 #define COLUMNAS 8
 
@@ -12,7 +26,7 @@ typedef Posicion** Tablero;
 
 void TableroInicializar(Tablero* tablero);
 void TableroImprimir(Tablero tablero);
-void TableroColocarPieza(Tablero*, Pieza);
+void TableroColocarPieza(Posicion*, Pieza*);
 void TableroDestruir(Tablero *tablero);
 
 #endif
--- src/tablero.c
+++ src/tablero.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<tablero.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <tablero.h>
 #include <posicion.h>
-#include<pieza.h>
+#include <pieza.h>
 
 #define CANTIDAD_PIEZAS 32
 
@@ -10,61 +11,20 @@ int TraducirColumnaAIndice(Columna columna);
 
 void TableroInicializar(Tablero* tablero)
 {
-    //Guasada monumental.
-    Pieza peonBlanco1, peonBlanco2, peonBlanco3, peonBlanco4, peonBlanco5, peonBlanco6, peonBlanco7, peonBlanco8, 
-    torreBlanco1, torreBlanco2, 
-    caballoBlanco1, caballoBlanco2, 
-    alfilBlanco1, alfilBlanco2, 
-    reyBlanco, reinaBlanco,
-    peonNegro1, peonNegro2, peonNegro3, peonNegro4, peonNegro5, peonNegro6, peonNegro7, peonNegro8, 
-    torreNegro1, torreNegro2, 
-    caballoNegro1, caballoNegro2,
-    alfilNegro1, alfilNegro2,
-    reyNegro, reinaNegro;    
-
-    
-    PiezaCrear(&torreBlanco1  , TORRE  , true, G, UNO);
-    PiezaCrear(&torreBlanco2  , TORRE  , true, G, OCHO);
-    PiezaCrear(&caballoBlanco1, CABALLO, true, G, DOS);
-    PiezaCrear(&caballoBlanco2, CABALLO, true, G, SIETE);
-    PiezaCrear(&alfilBlanco1  , ALFIL  , true, G, TRES);
-    PiezaCrear(&alfilBlanco2  , ALFIL  , true, G, SEIS);
-    PiezaCrear(&reyBlanco     , REY    , true, G, CUATRO);
-    PiezaCrear(&reinaBlanco   , REINA  , true, G, CINCO);
-
-    PiezaCrear(&peonBlanco1, PEON, true, H, UNO);
-    PiezaCrear(&peonBlanco2, PEON, true, H, DOS);
-    PiezaCrear(&peonBlanco3, PEON, true, H, TRES);
-    PiezaCrear(&peonBlanco4, PEON, true, H, CUATRO);
-    PiezaCrear(&peonBlanco5, PEON, true, H, CINCO);
-    PiezaCrear(&peonBlanco6, PEON, true, H, SEIS);
-    PiezaCrear(&peonBlanco7, PEON, true, H, SIETE);
-    PiezaCrear(&peonBlanco8, PEON, true, H, OCHO);
-
-    PiezaCrear(&peonNegro1, PEON, false, B, UNO);
-    PiezaCrear(&peonNegro2, PEON, false, B, DOS);
-    PiezaCrear(&peonNegro3, PEON, false, B, TRES);
-    PiezaCrear(&peonNegro4, PEON, false, B, CUATRO);
-    PiezaCrear(&peonNegro5, PEON, false, B, CINCO);
-    PiezaCrear(&peonNegro6, PEON, false, B, SEIS);
-    PiezaCrear(&peonNegro7, PEON, false, B, SIETE);
-    PiezaCrear(&peonNegro8, PEON, false, B, OCHO);
-    
-    PiezaCrear(&torreNegro1, TORRE, false, A, UNO);
-    PiezaCrear(&torreNegro2, TORRE, false, A, OCHO);
-    PiezaCrear(&caballoNegro1, CABALLO, false, A, DOS);
-    PiezaCrear(&caballoNegro2, CABALLO, false, A, SIETE);
-    PiezaCrear(&alfilNegro1, ALFIL, false, A, TRES);
-    PiezaCrear(&alfilNegro2, ALFIL, false, A, SEIS);
-    PiezaCrear(&reyNegro, REY, false, A, CUATRO);
-    PiezaCrear(&reinaNegro, REINA, false, A, CINCO);
-
-    Pieza piezas[CANTIDAD_PIEZAS] = {
-        torreBlanco1, torreBlanco2, caballoBlanco1, caballoBlanco2, alfilBlanco1, alfilBlanco2, reyBlanco, reinaBlanco,
-        peonBlanco1, peonBlanco2, peonBlanco3, peonBlanco4, peonBlanco5, peonBlanco6, peonBlanco7, peonBlanco8,
-        peonNegro1, peonNegro2, peonNegro3, peonNegro4, peonNegro5, peonNegro6, peonNegro7, peonNegro8,
-        torreNegro1, torreNegro2, caballoNegro1, caballoNegro2, alfilNegro1, alfilNegro2, reyNegro, reinaNegro
+    char tableroProvisorio[FILAS][COLUMNAS][3] = 
+    {
+        {TORRE_BLANCO, CABALLO_BLANCO, ALFIL_BLANCO, REY_BLANCO, REINA_BLANCO, ALFIL_BLANCO, CABALLO_BLANCO, TORRE_BLANCO},
+        {PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO, PEON_BLANCO},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {NADA, NADA, NADA, NADA, NADA, NADA, NADA, NADA},
+        {PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO, PEON_NEGRO},
+        {TORRE_NEGRO, CABALLO_NEGRO, ALFIL_NEGRO, REY_NEGRO, REINA_NEGRO, ALFIL_NEGRO, CABALLO_NEGRO, TORRE_NEGRO}
+        
     };
+    
+    Pieza *pieza;
 
     *tablero = (Tablero) malloc(FILAS * sizeof(Posicion*));
     int coordenada[2];
@@ -76,13 +36,17 @@ void TableroInicializar(Tablero* tablero)
         {
             coordenada[0] = x;
             PosicionInicializar(&((*tablero)[y][x]), coordenada);
+            
+            if (strcmp(tableroProvisorio[y][x], NADA) != 0)
+            {
+                pieza = (Pieza*)malloc(sizeof(Pieza));
+                PiezaCrear(pieza, tableroProvisorio[y][x][0], tableroProvisorio[y][x][1] == 'B');
+                TableroColocarPieza(&(*tablero)[y][x], pieza);
+            }
+            
         }
     }
 
-    for(int i = 0; i < CANTIDAD_PIEZAS; i++)
-    {
-        TableroColocarPieza(tablero, piezas[i]);
-    }
 }
 
 void __imprimirLineaHorizontal(size_t largo)
@@ -122,10 +86,10 @@ void TableroImprimir(Tablero tablero)
     }
 }
 
-void TableroColocarPieza(Tablero* tablero, Pieza pieza) {
-    int columnaIndice = TraducirColumnaAIndice(pieza.columnaActual);
-    (*tablero)[pieza.filaActual - 1][columnaIndice].ranura = (Pieza*)malloc(sizeof(Pieza));
-    PiezaCopiar((*tablero)[pieza.filaActual - 1][columnaIndice].ranura, pieza);
+void TableroColocarPieza(Posicion* posicion, Pieza *pieza)
+{
+    posicion->ranura = pieza;
+    PiezaEsColocada(pieza, posicion);
 }
  
 void TableroDestruir(Tablero *tablero)
@@ -142,6 +106,7 @@ void TableroDestruir(Tablero *tablero)
     free(*tablero);
 }
 
-int TraducirColumnaAIndice(Columna columna) {
+int TraducirColumnaAIndice(Columna columna)
+{
     return (int)(columna - 'A');
 }
