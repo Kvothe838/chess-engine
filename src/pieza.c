b0dbc2225a3ca10a3d3cec374b8a503fb659ecc1
Kvothe838 <facundoyuffrida@gmail.com>
2020-01-16 15:47:13 -0300

    Todo terminado. Falta que no crashee por algo turbio del malloc.

--- src/pieza.c
+++ src/pieza.c
@@ -14,12 +14,133 @@ void __PiezaCrearCoordenadas(Coordenada** coordenadas, int len, int** coordenada
     }
 }
 
+void __PiezaLiberarValoresCoordenadas(int len, int** coordenadasEnNumero) {
+    for (int i = 0; i < len; i++) {
+        free(coordenadasEnNumero[i]);
+    }
+
+    free(coordenadasEnNumero);
+}
+
+void __ValoresCoordenadasInicializar(int** valoresCoordenadas, int cantidadMovimientos) {
+    (*valoresCoordenadas) = (int*) malloc(cantidadMovimientos * sizeof(int));
+}
+
+void __ValoresCoordenadasLlenar_Rey(int** valoresCoordenadas) {
+    (*valoresCoordenadas)[0] = (*valoresCoordenadas)[2] = (*valoresCoordenadas)[5] = (*valoresCoordenadas)[7] = 0;
+    (*valoresCoordenadas)[1] = (*valoresCoordenadas)[4] = (*valoresCoordenadas)[8] = (*valoresCoordenadas)[9] = (*valoresCoordenadas)[10] = (*valoresCoordenadas)[13] = 1;
+    (*valoresCoordenadas)[3] = (*valoresCoordenadas)[6] = (*valoresCoordenadas)[11] = (*valoresCoordenadas)[12] = (*valoresCoordenadas)[14] = (*valoresCoordenadas)[15] = -1;
+/*  Coordenadas más visibles 
+    (0,1), 
+    (0,-1), 
+    (1,0), 
+    (-1,0), 
+    (1,1), 
+    (1,-1), 
+    (-1,1), 
+    (-1,-1)  */
+}
+
+void __ValoresCoordenadasLlenar_Reina(int** valoresCoordenadas) {
+    for(int i = 0; i < 8; i++) {
+        for(int j = i*7; j < (i+1)*7; j++) {
+            if(j >= 0 && j < 7) {
+                (*valoresCoordenadas)[2*j] = 0;
+                (*valoresCoordenadas)[2*j+1] = j+1;
+            } else if(j >= 7 && j < 14) {
+                (*valoresCoordenadas)[2*j] = 0;
+                (*valoresCoordenadas)[2*j+1] = 6-j;
+            } else if(j >= 14 && j < 21) {
+                (*valoresCoordenadas)[2*j] = j-14;
+                (*valoresCoordenadas)[2*j+1] = 0;
+            } else if(j >= 21 && j < 28) {
+                (*valoresCoordenadas)[2*j] = 21-j;
+                (*valoresCoordenadas)[2*j+1] = 0;
+            } else if (j >= 28 && j < 35) {
+                (*valoresCoordenadas)[2*j] = j-27;
+                (*valoresCoordenadas)[2*j+1] = j-27;
+            } else if (j >= 35 && j < 42) {
+                (*valoresCoordenadas)[2*j] = 34-j;
+                (*valoresCoordenadas)[2*j+1] = 34-j;
+            } else if (j >= 42 && j < 49) {
+                (*valoresCoordenadas)[2*j] = j-41;
+                (*valoresCoordenadas)[2*j+1] = 41-j;
+            } else if (j >= 49 && j < 56) {
+                (*valoresCoordenadas)[2*j] = 48-j;
+                (*valoresCoordenadas)[2*j+1] = j-48;
+            }
+        }                
+    }
+}
+
+void __ValoresCoordenadasLlenar_Torre(int** valoresCoordenadas) {
+    for(int i = 0; i < 4; i++) {
+        for(int j = i*7; j < (i+1)*7; j++) {
+            if(j >= 0 && j < 7) {
+                (*valoresCoordenadas)[2*j] = 0;
+                (*valoresCoordenadas)[2*j+1] = j+1;
+            } else if(j >= 7 && j < 14) {
+                (*valoresCoordenadas)[2*j] = 0;
+                (*valoresCoordenadas)[2*j+1] = 6-j;
+            } else if(j >= 14 && j < 21) {
+                (*valoresCoordenadas)[2*j] = j-14;
+                (*valoresCoordenadas)[2*j+1] = 0;
+            } else if(j >= 21 && j < 28) {
+                (*valoresCoordenadas)[2*j] = 21-j;
+                (*valoresCoordenadas)[2*j+1] = 0;
+            }
+        }                
+    }
+}
+
+void __ValoresCoordenadasLlenar_Alfil(int** valoresCoordenadas) {
+    for(int i = 0; i < 4; i++) {
+        for(int j = i*7; j < (i+1)*7; j++) {
+            if (j >= 28 && j < 35) {
+                (*valoresCoordenadas)[2*j] = j-27;
+                (*valoresCoordenadas)[2*j+1] = j-27;
+            } else if (j >= 35 && j < 42) {
+                (*valoresCoordenadas)[2*j] = 34-j;
+                (*valoresCoordenadas)[2*j+1] = 34-j;
+            } else if (j >= 42 && j < 49) {
+                (*valoresCoordenadas)[2*j] = j-41;
+                (*valoresCoordenadas)[2*j+1] = 41-j;
+            } else if (j >= 49 && j < 56) {
+                (*valoresCoordenadas)[2*j] = 48-j;
+                (*valoresCoordenadas)[2*j+1] = j-48;
+            }
+        }                
+    }
+}
+
+void __ValoresCoordenadasLlenar_Caballo(int** valoresCoordenadas) {
+    (*valoresCoordenadas)[0] = (*valoresCoordenadas)[2] = (*valoresCoordenadas)[5] = (*valoresCoordenadas)[7] = 2;
+    (*valoresCoordenadas)[1] = (*valoresCoordenadas)[4] = (*valoresCoordenadas)[8] = (*valoresCoordenadas)[15] = 1;
+    (*valoresCoordenadas)[3] = (*valoresCoordenadas)[6] = (*valoresCoordenadas)[10] = (*valoresCoordenadas)[13] = -1;
+    (*valoresCoordenadas)[9] = (*valoresCoordenadas)[11] = (*valoresCoordenadas)[12] = (*valoresCoordenadas)[14] = -2;
+/*  Coordenadas más visibles 
+    (2,1),     
+    (2,-1), 
+    (1,2),
+    (-1,2), 
+    (1,-2),     
+    (-1,-2), 
+    (-2,-1), 
+    (-2,1)  */
+}
+
+void __ValoresCoordenadasLlenar_Peon(int** valoresCoordenadas) {
+    (*valoresCoordenadas)[0] = 0;
+    (*valoresCoordenadas)[1] = 1;
+}
+
 void __PiezaRepresentacionYMovimientos(Pieza* pieza, TipoPieza tipo)
 {
     setlocale(LC_ALL, "");
     int base = 0x2654;
     Coordenada* coordenadas;
-    int * posiciones;
+    int* valoresCoordenadas;
+    int cantidadMovimientos;
 
     if(pieza->esBlanca)
     {
@@ -29,78 +150,56 @@ void __PiezaRepresentacionYMovimientos(Pieza* pieza, TipoPieza tipo)
     switch (tipo) {
         case REY:
             base += 0;
+            cantidadMovimientos = 16;
+            __ValoresCoordenadasInicializar(&valoresCoordenadas, cantidadMovimientos);
+            __ValoresCoordenadasLlenar_Rey(&valoresCoordenadas);
             
-            posiciones = (int*) malloc(16 * sizeof(int));
-            posiciones[0] = posiciones[2] = posiciones[5] = posiciones[7] = 0;
-            posiciones[1] = posiciones[4] = posiciones[8] = posiciones[9] = posiciones[10] = posiciones[13] = 1;
-            posiciones[3] = posiciones[6] = posiciones[11] = posiciones[12] = posiciones[14] = posiciones[15] = -1;
-            //Coordenadas más visibles 
-        /*  (0,1), 
-            (0,-1), 
-            (1,0), 
-            (-1,0), 
-            (1,1), 
-            (1,-1), 
-            (-1,1), 
-            (-1,-1)  */
-            __PiezaCrearCoordenadas(&coordenadas, 16, &posiciones);
             break;
 
         case REINA:
             base += 1;
-            posiciones = (int*) malloc(56 * sizeof(int));
+            cantidadMovimientos = 56;
+            __ValoresCoordenadasInicializar(&valoresCoordenadas, cantidadMovimientos);
+            __ValoresCoordenadasLlenar_Reina(&valoresCoordenadas);            
 
-            for(int i = 0; i < 7; i++) {
-                for(int j = i*7; j < (i+1)*7; j++) {
-                    if(j >= 0 && j < 7) {
-                        posiciones[2*j] = 0;
-                        posiciones[2*j+1] = j+1;
-                    } else if(j >= 7 && j < 14) {
-                        posiciones[2*j] = 0;
-                        posiciones[2*j+1] = 6-j;
-                    } else if(j >= 14 && j < 21) {
-                        posiciones[2*j] = j-14;
-                        posiciones[2*j+1] = 0;
-                    } else if(j >= 21 && j < 28) {
-                        posiciones[2*j] = 21-j;
-                        posiciones[2*j+1] = 0;
-                    } else if (j >= 28 && j < 35) {
-                        posiciones[2*j] = j-27;
-                        posiciones[2*j+1] = j-27;
-                    } else if (j >= 35 && j < 42) {
-                        posiciones[2*j] = 34-j;
-                        posiciones[2*j+1] = 34-j;
-                    } else if (j >= 42 && j < 49) {
-                        posiciones[2*j] = j-41;
-                        posiciones[2*j+1] = 41-j;
-                    } else if (j >= 49 && j < 56) {
-                        posiciones[2*j] = 48-j;
-                        posiciones[2*j+1] = j-48;
-                    }
-                }                
-            }
-
-            __PiezaCrearCoordenadas(&coordenadas, 56, &posiciones);
             break;
 
         case TORRE:
             base += 2;
+            cantidadMovimientos = 28;
+            __ValoresCoordenadasInicializar(&valoresCoordenadas, cantidadMovimientos);
+            __ValoresCoordenadasLlenar_Torre(&valoresCoordenadas);
+
             break;
 
         case ALFIL:
             base += 3;
+            cantidadMovimientos = 28;
+            __ValoresCoordenadasInicializar(&valoresCoordenadas, cantidadMovimientos);
+            __ValoresCoordenadasLlenar_Alfil(&valoresCoordenadas);
+
             break;
 
         case CABALLO:
             base += 4;
+            cantidadMovimientos = 8;
+            __ValoresCoordenadasInicializar(&valoresCoordenadas, cantidadMovimientos);
+            __ValoresCoordenadasLlenar_Caballo(&valoresCoordenadas);
+
             break;
 
         case PEON:
             base += 5;
+            cantidadMovimientos = 1;
+            __ValoresCoordenadasInicializar(&valoresCoordenadas, cantidadMovimientos);
+            __ValoresCoordenadasLlenar_Peon(&valoresCoordenadas);
+            //TODO: Devolver falta el movimiento de la primer fila 
+
             break;   
     }
 
-    //Liberar posiciones.
+    __PiezaCrearCoordenadas(&coordenadas, cantidadMovimientos, &valoresCoordenadas);
+    __PiezaLiberarValoresCoordenadas(cantidadMovimientos, &valoresCoordenadas);
     
     pieza->representacion = base;
     pieza->coordenadasMovimientosPosibles = coordenadas;
