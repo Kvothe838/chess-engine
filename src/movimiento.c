12f59b3142f0e6ce208a86904e874c77940c2b94
MauricioFIUBA <mlcabrera@fi.uba.ar>
2020-12-21 22:52:22 -0300

    Intento de bloqueo de movimiento incompleto

--- src/movimiento.c
+++ src/movimiento.c
@@ -101,3 +101,76 @@ status_t MoverPieza(Tablero* tablero, Casilla casilla, Posicion* posicionInicial
     TableroColocarPieza(posicionFinal, pieza);
     return ST_OK;
 }
+
+/*bloquearSaltosDeCasillas(*juego, &casillas, &cantidadPosiblesMovimientos, *(piezasPosibles[0]->posicion->casilla));*/
+
+void bloquearSaltosDeCasillas(Juego juego, Casilla** casillas, int* cantidadPosiblesMovimientos, Casilla casillaInicial)
+{
+    /*
+    Lo que tendria que hacer es escribir un programa que detecte casillas con piezas 
+    (corregir que no son jugadores), una vez que las dectecto, tengo que saber si
+    las piezas estan una fila, columna, o diagonal (directa o cruzada) de la pieza
+    principal. Una vez que se esto, tengo que obtener eltestJuego.tablero valor de la diferencia
+    y me tengo que quedar que el menor valor positivo y el mayor valor negativo de cada uno.
+    Por ejemplo, comparar la diferencia de las filas de todas las posiciones con piezas
+    y quedarme con la cota minima para saber cuales coordenadas con piezas esta mas cerca
+    y una vez que se esto, filtro con todas las piezas.
+    */
+    if ((*cantidadPosiblesMovimientos) == 0)
+    {
+        return;
+    }
+    Posicion* chequeoDePosicion;
+    Casilla* casillasFiltradas = NULL, *casillasConJugadores = NULL, casillaConJugador;
+    size_t cantidadDeCasillasFiltradas = 0, cantidadDeCasillasConJugadores = 0;
+    orientacion_t orientacion;
+    for (int i = 0; i < *cantidadPosiblesMovimientos; ++i)
+    {
+        chequeoDePosicion = TableroObtenerPieza(juego.tablero, (*casillas)[i]);
+        if (chequeoDePosicion->ranura != NULL)
+        {
+            casillasConJugadores = (Casilla*) realloc(casillasConJugadores, (cantidadDeCasillasConJugadores+1) * sizeof(Casilla));
+            casillasConJugadores[cantidadDeCasillasConJugadores++] = *(chequeoDePosicion->casilla);
+        
+            casillaConJugador = casillasConJugadores[cantidadDeCasillasConJugadores-1];
+            if (casillaInicial.columna == casillaConJugador.columna)
+            {
+                orientacion = ORIENT_COLUMNA;
+
+            }
+            else if (casillaInicial.fila == casillaConJugador.fila)
+            {
+                orientacion = ORIENT_FILA;
+            }
+
+            if ((Fila)(casillaInicial.columna - casillaConJugador.columna) == (casillaInicial.fila == casillaConJugador.fila))
+            {
+                orientacion = ORIENT_DIAG_IGUAL;
+            }
+            else if ((Fila)(casillaInicial.columna - casillaConJugador.columna) == -1*(casillaInicial.fila == casillaConJugador.fila))
+            {
+                orientacion = ORIENT_DIAG_CRUZ;
+            }
+
+        }
+
+    }
+    /*
+    Parece redudnate el ultimo else ya que casi se probaron todos lo casos,
+    pero de esta forma se chequea todos los casos de movimientos por filas
+    y columnas y permite que el caballo siga saltando
+    */
+
+    switch (orientacion) 
+    {
+        case ORIENT_COLUMNA:
+
+
+        default:
+            printf("%s\n", "No tendria que llegar aca");
+    }
+
+
+    *cantidadPosiblesMovimientos = cantidadDeCasillasFiltradas;
+    *casillas = casillasFiltradas;
+}
